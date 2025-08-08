f30b88a2d165b7b7b244a986d0353f872c8fcb46
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-08-08 15:07:21 +0600

    modified Problems 01

--- Problems/P1.asm
+++ Problems/P1.asm
@@ -1,81 +1,73 @@
-.model small 
+.model small
 .stack 100h
 .data
 msg db 'Enter any character: $'
-msg1 db 'Yor have entered: $' 
-lower db 'Lower case is: $'
-upper db 'Upper case is: $'
-char db ?, '$'
 nl db 13, 10, '$'
+display db 13, 10, 'you have entered: $'
+lower db 13, 10, 'Lower is: $'
+upper db 13, 10, 'Upper is: $' 
+char db ?
 .code
-main proc
+main proc 
     mov ax, @data
     mov ds, ax
     
-    ;Print msg
+    ; Print msg
     lea dx, msg
     mov ah, 9
     int 21h
-    
-    ;Enter char
+    ; Take input store in char var
     mov ah, 1
     int 21h
-    mov char, al
-    ;Print newline
-    lea dx, nl
+    mov char, al 
+    ; Display input char
+    lea dx, display
     mov ah, 9
     int 21h
     
-    ;Print msg1
-    lea dx, msg1
-    mov ah, 9
-    int 21h
-    ;Print char 
-    lea dx, char
-    mov ah, 9
-    int 21h
+    mov ah, 2 
+    mov dl, char
+    int 21h     
     
-    ;Print newline
-    lea dx, nl
-    mov ah, 9
-    int 21h 
-    
-    ;Check lower or upper
-    cmp char, 'a' ;if (char>'a') cout<< upper
+    ; Check if (char<'a')
+    cmp char, 'a'
     jl check_upper
-    cmp char, 'z'
-    jg check_upper
     
-    ;Print upper
-    lea dx, upper
-    mov ah, 9
-    int 21h
+    ; else 
+    sub char, 32 ; Convert to upper
+    jmp result
     
-    
-    sub char, 32 ; Convert lower to upper
-    jmp print_result
     
     check_upper:
         cmp char, 'A'
         jl exit
-        cmp char, 'Z'
-        jg exit
-        ;Print lower
+       
+        
         lea dx, lower
         mov ah, 9
         int 21h
-        
-        
-        add char, 32 ; Convert upper to lower
-    print_result:
-        mov dl, char
-        mov ah, 2
-        int 21h
+        ; Print lower
+        add char, 32 ; Convert to lower
+         mov ah, 2
+         mov dl, char
+         int 21h
         jmp exit
-    exit:
+
+      ; Pritn upper   
+     result:
+
+        lea dx, upper 
+        mov ah, 9
+        int 21h
+        mov ah, 2
+        mov dl, char
+        int 21h
+        
+      exit:
         mov ah, 4ch
         int 21h
-        
+    
+    
     
 main endp
 end main
