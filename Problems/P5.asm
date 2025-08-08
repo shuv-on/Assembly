60a5e3e27bfd4a6b672b2c8512db3dc6cb5be52d
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-08-08 16:44:44 +0600

    Modified P6

--- Problems/P5.asm
+++ Problems/P5.asm
@@ -1,21 +1,36 @@
 .model small
 .stack 100h
+.data
+nl db 13, 10, '$'
+count db 0 ; count=0
 .code
 main proc
-     mov bl,30h
-    lp:
-       mov ah,1
-       int 21h
-       inc bl
-       cmp al,20h
-       je exit
-       jmp lp
-    exit:
-        sub bl,31h
-        add bl,30h
-        mov ah,2
-        mov dl,bl
+    mov ax, @data
+    mov ds, ax
+    
+    loop:
+        mov ah, 1
+        int 21h
+        cmp al, ' '
+        je output
+        inc count ; count++
+        jmp loop 
+        
+     output:
+        lea dx, nl
+        mov ah, 9
         int 21h
         
-        main endp
-    end main
+        ; Convert num ascii
+        mov al, count
+        add al, '0' ; decimal to ascii
+        mov dl, al
+        mov ah, 2
+        int 21h
+        jmp exit
+     
+     exit:
+        mov ah, 4ch
+        int 21h
+main endp
+end main
