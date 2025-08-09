02a5a7e0a940d10f033c5687bf2b1313b98695ef
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-08-09 07:53:48 +0600

    modified P10

---
+++ Problems/P10m.asm
@@ -0,0 +1,26 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+   mov cx,0
+   mov ah,1
+input_loop:
+   cmp al,13
+   je end_loop
+   push ax
+   inc cx
+   int 21h
+   jmp input_loop
+end_loop:
+   jcxz exit
+   mov ah,2
+print:
+   pop dx
+   int 21h
+   loop print
+exit:
+   mov ah,4Ch
+   int 21h
+main endp
+end main
